522cbf040deafd02226a8c4f3360b16878c8d0cc
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-04-02 12:57:59 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// question 1
+
+const cat = {
+  property1: "complain",
+  catSounds: function () {
+    //return "meow!";
+  }
+};
+console.log("meow!");
+
+
+// Question 2 and 3 
+
+const heading = document.querySelector("h3");
+heading .innerHTML =  "Updated heading";
+
+heading.style.fontSize= "2em";
+
+
+//Question 4
+
+const heading = document.querySelector("h3")
+console.log(heading.className);
+
+heading.className = "subheading";
+
+console.log(heading.className);
+
