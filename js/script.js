067560cd0e0eb53dd6ebe51ab025d726338a0fb3
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-04-02 15:29:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,3 +48,31 @@ console.log(resultsContainer.innerHTML);
 resultsContainer .innerHTML +=`<p>New paragraph</p>`;
 
 console.log(resultsContainer.innerHTML);
+
+//question 7
+
+
+
+const cats = [
+  {
+      name: "Blob",
+      age: 10
+  },
+  {
+      name: "Harold",
+  },
+  {
+      name: "Blurt",
+      age: 21
+  }
+];
+
+
+function animal(){
+
+for (let i = 0 ;  i <cats.length;  i++){
+
+}
+};
+
+console.log([0].age);
